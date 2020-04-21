8f8ac0824927e3d23574f68e0b1b2d00aa3ece00
Hsin Ling Hu <hsinkava11@gmail.com>
2020-04-21 11:34:35 -0700

    click like is working

--- src/component/App/App.js
+++ src/component/App/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import {Route, Switch } from 'react-router-dom';
 import Landing from '../Landing/Landing';
 import './App.css';
+import DashboardPage from '../Dashboard/DashboardPage';
 
 export default class App extends Component {
   render() {
@@ -10,6 +11,7 @@ export default class App extends Component {
      <header><h1>Memestagram</h1></header>
      <Switch>
      <Route exact path='/' component={Landing}/>
+     <Route exact path='/dashboard' component={DashboardPage}/>
      {/*<Route path='/login' component={Login}/>*/}
      {/*<Route path='/register' component={RegisterationPage}/>*/}
      {/*<Route component={NotFound}/>*/}
--- src/component/Dashboard/DashboardPage.js
+++ src/component/Dashboard/DashboardPage.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
+import DashPost from './DashPost';
 
 export default class DashboardPage extends Component {
+    state={
+       posts:[]
+    }
     render() {
         return (
             <div>
-                <div className='profImg'>Image</div><span>Username</span>
-                <div className='MemePost'>Meme post</div>
-                <span>❤️like: 420</span><span>Comment: 6 </span>
+                <DashPost />
             </div>
         )
     }
