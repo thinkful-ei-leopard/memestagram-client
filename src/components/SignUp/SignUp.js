dec99760f2855a0b0a884e3b28b6cc8c965bd998
shimmy77 <christianshim@gmail.com>
2020-04-21 12:25:04 -0500

    signup almost

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import { Input, Required, Label } from '../Form/Form'
+import AuthApiService from '../../services/auth-api-service'
+import Button from '../Button/Button'
 
 export default class SignUp extends Component { 
     static defaultProps = {
@@ -12,14 +14,80 @@ export default class SignUp extends Component {
     handleSubmit = ev => {
         ev.preventDefault()
         const { name, username, password } = ev.target
-        
-    }
+        AuthApiService.postUser({
+            name: name.value,
+            username: username.value,
+            password: password.value,
+          })
+            .then(user => {
+              name.value = ''
+              username.value = ''
+              password.value = ''
+              this.props.onRegistrationSuccess()
+            })
+            .catch(res => {
+              this.setState({ error: res.error })
+            })
+        }
+      
+        componentDidMount() {
+          this.firstInput.current.focus()
+        }
 
     render() {
-       
+       const { error } = this.state
         return (
+            <form
+        className='form'
+        onSubmit={this.handleSubmit}
+      >
+        <div role='alert'>
+          {error && <p>{error}</p>}
+        </div>
+        <div>
+          <Label htmlFor='registration-name-input'>
+            Enter your name<Required />
+          </Label>
+          <Input
+            ref={this.firstInput}
+            id='registration-name-input'
+            name='name'
+            required
+          />
+        </div>
+        <div>
+          <Label htmlFor='registration-username-input'>
+            Choose a username<Required />
+          </Label>
+          <Input
+            id='registration-username-input'
+            name='username'
+            required
+          />
+        </div>
+        <div>
+          <Label htmlFor='registration-password-input'>
+            Choose a password<Required />
+          </Label>
+          <Input
+            id='registration-password-input'
+            name='password'
+            type='password'
+            required
+          />
+        </div>
+        <footer className='form-footer'>
+          <Button type='submit' className='form-footer-button'>
+            Sign up
+          </Button>
+          {' '}
+          <Link to='/login' className='form-footer-link'>Already have an account?</Link>
+        </footer>
+      </form>
             
             
         )
     }
 }
+
+export default SignUp
