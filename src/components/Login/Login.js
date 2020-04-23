35ce6781fb68fd20844d4c1af0c02257a03b0280
Poden <56049210+Poden@users.noreply.github.com>
2020-04-23 13:10:42 -0500

    login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react'
 import Button from '../Button/Button'
+import { Input, Label } from '../Form/Form'
+
+
 class Login extends Component{
 
   render() {
     
     return (
-      <form>
+      <form className='LoginForm' onSubmit={this.handleSubmit}>
+
         <div className='login-input'>
-          <label htmlFor='login-username'> Username </label>
-          <input id='login-username' name='username' required />
+          <Label htmlFor='login-username'> Username </Label>
+          <Input ref={this.firstInput} id='login-username' name='username' required />
         </div>
         <div className='login-input'>
-          <label htmlFor='login-password'>Password</label>
-          <input id='login-password' name='password' type='password' required/>
+          <Label htmlFor='login-password'>Password</Label>
+          <Input id='login-password' name='password' type='password' required/>
         </div>
         <div className='login-input'>
         <Button type='submit'> Login </Button>
