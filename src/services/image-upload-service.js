ba9befc59299ede172d457082425bd02186833a3
shimmy77 <christianshim@gmail.com>
2020-04-27 14:25:09 -0500

    trying to merge

--- src/services/image-upload-service.js
+++ src/services/image-upload-service.js
@@ -2,7 +2,7 @@ import config from '../config'
 
 const ImageUploadService = {
   postProfileImage(formData) {
-    return fetch(`${config.API_ENDPOINT}/imageupload`, {
+    return fetch(`${config.API_ENDPOINT}/image-upload`, {
       method: 'POST',
       body: formData 
     })
