0839f2d99c3786eae4229c5860076bcbcd11688a
shimmy77 <christianshim@gmail.com>
2020-04-29 13:57:54 -0500

    getting it done

---
+++ src/components/UserSettings/UserSettings.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { Input, Label } from '../Form/Form'
+import Button from '../Button/Button'
+
+export default class UserSetting extends Component {
+    render() {
+        return (
+            <div>
+                <Navbar />
+                
+            </div>
+        )
+    }
+}
+
